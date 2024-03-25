ff74a10e614fa72a5cee308e3d96615cdcecde26
RealWhimsy <lukas.jack@web.de>
2024-03-25 17:27:42 +0100

    added SuperMain, needed as entry point for jar

---
+++ src/main/java/de/realwhimsy/afktraderpoe/SuperMain.java
@@ -0,0 +1,10 @@
+package de.realwhimsy.afktraderpoe;
+
+import de.realwhimsy.afktraderpoe.AFKPoeTraderApplication;
+
+public class SuperMain {
+
+    public static void main(String[] args) {
+        AFKPoeTraderApplication.main(null);
+    }
+}
